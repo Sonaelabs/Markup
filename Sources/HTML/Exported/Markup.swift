01d477f3b94c1d589d6e86afc4f828623c879366
Felix Gabel <felix.gabel@me.com>
2026-08-01 19:32:07 +1000

    Export ContentBuilder from HTML
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

--- Sources/HTML/Exported/Markup.swift
+++ Sources/HTML/Exported/Markup.swift
@@ -5,6 +5,8 @@
 
 @_exported import protocol Markup.StreamWriter
 
+@_exported import struct Markup.ContentBuilder
+
 @_exported import struct Markup.Attribute
 @_exported import struct Markup.AsyncContent
 @_exported import struct Markup.AsyncForEach
